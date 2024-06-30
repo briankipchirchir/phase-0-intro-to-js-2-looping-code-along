1c86041f710f289bb0e7179f55e626fcbb6f5293
briankipchirchir <kbrianngetich@gmail.com>
2024-06-30 18:15:23 +0300

    phase-0-intro-to-js-2-looping-code-along

--- index.js
+++ index.js
@@ -1,1 +1,50 @@
-// Code your solutions in this file
+function output(countdown) {
+    while(countdown<11){
+        console.log(countdown--);
+    if (countdown<0){
+        break;
+        return output;
+    }
+    }
+    
+}
+output(10);
+
+//
+names=(["Charlie", "Samip", "Ali"]);
+
+
+function writeCards(names, birthday) {
+
+    const messages = [];
+  
+    for (let i = 0; i < names.length; i++) {
+      const name = names[i];
+  
+      const message = `Thank you for attending our ${eventName} event, ${name}!`;
+  
+      
+      messages.push(Today);
+    }
+  
+    
+    return messages;
+  }
+
+
+  const attendeeNames = ["Alice", "Bob", "Charlie"];
+const event = "Company Picnic";
+
+function writeCards() {
+
+    for (let i = 0; i < attendeeNames.length; i++) {
+        const element = array[i];
+        console.log(thankYou &attendeeNames[i]);
+        
+    }
+    
+}
+
+const thankYouCards = writeCards(attendeeNames, event);
+
+console.log(thankYouCards);
